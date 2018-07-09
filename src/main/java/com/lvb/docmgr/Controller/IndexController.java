f1267d09b07c637371253d81e689eaa962b461a3
hxf <ginah232@outlook.com>
2018-07-09 23:11:25 +0800

    hxf提交的注册登陆注销功能

--- src/main/java/com/lvb/docmgr/Controller/IndexController.java
+++ src/main/java/com/lvb/docmgr/Controller/IndexController.java
@@ -1,4 +1,17 @@
 package com.lvb.docmgr.Controller;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
 public class IndexController {
+    @RequestMapping("/index")
+    public String index(ModelMap modelMap, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if(user!=null){
+            modelMap.addAttribute("username",user.getUsername());
+        }
+        return "index";
+    }
 }
