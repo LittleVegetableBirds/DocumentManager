1909614b6e010dbd798de5ea6d53834452890a1f
hp <13581726353@163.com>
2018-07-09 23:19:48 +0800

    user

--- src/main/java/com/lvb/docmgr/Controller/UserController.java
+++ src/main/java/com/lvb/docmgr/Controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -61,6 +62,16 @@ public class UserController {
             }else{
                 session.setAttribute("user",user);
                 return "redirect:/index";
+            }
+            @RequestMapping(path = "/user/check", method = RequestMethod.POST)
+            @ResponseBody
+            String checkUser(@RequestParam("username") String username) {
+                User user = userService.getUserByUsername(username);
+                if (user != null) {
+                    return "invalid";
+                } else {
+                    return "valid";
+                }
             }
         }
     }
